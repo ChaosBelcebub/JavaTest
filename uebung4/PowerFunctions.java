f5d191d1f9669c9c62307ebad9fc6bf81e05625f
ChaosBelcebub <mich3ael@web.de>
2015-09-04 12:26:25 +0200

    add the square multiply algorithm

--- uebung4/PowerFunctions.java
+++ uebung4/PowerFunctions.java
@@ -18,6 +18,28 @@ public class PowerFunctions
 
   public static double fastPower(double x, int n)
   {
-    return 0;
+    double result = 1;
+    String b = Integer.toBinaryString(n);
+    for (int i = 0; i < b.length(); i++)
+    {
+      result = (result * result);
+      if (b.charAt(i) == '1')
+      {
+        result = result * x;
+      }
+    }
+    return result;
+  }
+
+  
+  /**
+  * Main
+  *
+  * @param args parameter
+  */
+  public static void main(String[] args)
+  {
+    System.out.println(Double.toString(power(4, 5)));
+    System.out.println(Double.toString(fastPower(4, 5)));
   }
 }
